81ef6367b694dc6ee22216a9ae3d6c595f263888
danielalas <danielalas@localhost>
2023-02-16 18:49:45 -0600

    Subiendo pruebas unitarias

---
+++ src/test/java/com/anahuac/calidad/unittest/TestCalculadora.java
@@ -0,0 +1,87 @@
+package com.anahuac.calidad.unittest;
+
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCalculadora {
+
+	private Calculadora calculadora;
+	
+	@Before
+	public void setup() {
+		calculadora = new Calculadora();
+		System.out.println("Este es el before");
+	}
+	
+	@Test
+	public void testSuma() {
+		double resultadoEsperado = 5;
+		
+		double resultadoEjecucion = calculadora.suma(2, 3);
+		
+		assertThat(resultadoEjecucion, equalTo(resultadoEsperado));
+	}
+	
+	@Test
+	public void testResta() {
+		double resultadoEsperado = 5;
+		
+		double resultadoEjecucion = calculadora.resta(10, 5);
+		
+		assertThat(resultadoEjecucion, equalTo(resultadoEsperado));
+	}
+	
+	@Test
+	public void testDivision() {
+		double resultadoEsperado = 5;
+		
+		double resultadoEjecucion = calculadora.division(10, 2);
+		
+		assertThat(resultadoEjecucion, equalTo(resultadoEsperado));
+	}
+	
+	@Test
+	public void testDivisionEntreCero() {
+		
+		double resultadoEsperado = Double.POSITIVE_INFINITY;
+		
+		double resultadoEjecucion = calculadora.division(10, 0);
+		
+		assertThat(resultadoEjecucion, equalTo(resultadoEsperado));
+		
+	}
+	
+	@Test
+	public void testDivisionEntreNegativo() {
+		double resultadoEsperado = -2;
+		
+		double resultadoEjecucion = calculadora.division(10, -5);
+		
+		assertThat(resultadoEjecucion, equalTo(resultadoEsperado));
+	}
+	
+	@Test
+	public void testMultiplicacion() {
+		double resultadoEsperado = 10;
+		
+		double resultadoEjecucion = calculadora.multiplicacion(2, 5);
+		
+		assertThat(resultadoEjecucion, equalTo(resultadoEsperado));
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testDivisionEntera() {
+		int resultadoEjecucion = calculadora.divisionEntera(10, 0);
+
+	}
+	
+	@After
+	public void clean() {
+		System.out.println("After");
+	}
+
+}
